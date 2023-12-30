9693d17682806d71e6633ffaedd263bf5c555521
Devang Bondre <shinigamiryuk389@gmail.com>
2023-12-31 04:32:39 +0530

    code files has been updated

--- script.js
+++ script.js
@@ -1,44 +1,92 @@
-function getComputerChoice(){
-    let computerChoice = ['ROCK', 'PAPER', 'SCISSORS'];
-    let randomComputerChoice = Math.floor(Math.random() * computerChoice.length);
-    return computerChoice[randomComputerChoice]; 
+
+const buttons = document.querySelectorAll(".btn")
+const playerChoice = document.getElementById("playerChoice")
+const computerChoice = document.getElementById("computerChoice")
+const result = document.getElementById("result")
+
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach((button) =>{
+console.log("Button click event added");
+button.addEventListener('click',playRound)
+});
+
+
+function playRound(e){
+
+  console.log("Play round called");
+  const playerMove = this.id;
+  const computerMove = computerPlay()
+
+
+  console.log("Player move:", playerMove);
+  console.log("Computer move:", computerMove);
+
+  if(playerScore != 3 || computerScore != 3 ){
+    if(computerMove === playerMove){
+        playerChoice.textContent = "You picked : " + playerMove.toUpperCase(); 
+        computerChoice.textContent = "You picked : " + computerMove 
+        result.textContent = "Its a Tie ! "
+
+    }
+    else if((playerMove === "rock") && (computerMove === "SCISSORS") ||
+    (playerMove === "paper") && (computerMove === "ROCK")||
+    (playerMove === "scissors" && (computerMove === "PAPER")) 
+    ){
+        playerChoice.textContent = "You picked : " + playerMove.toUpperCase();
+        computerChoice.textContent = "You picked : " + computerMove 
+        result.textContent = "Congratulations You Won The Round ! "
+        playerScore++;
+        
+    }
+
+    else if((computerMove === "ROCK") && (playerMove === "scissors") ||
+    (computerMove === "PAPER") && (playerMove === "rock") ||
+    (computerMove === "SCISSORS") && (playerMove === "paper")
+    ){
+        playerChoice.textContent = "You picked : " + playerMove.toUpperCase();
+        computerChoice.textContent ="You picked : " + computerMove 
+        result.textContent = "Sorry You Lost The Round ! "
+        computerScore++;
+        
+    }
+    updateScores();
+    if (playerScore === 3 || computerScore === 3) {
+      endGame();
+    }
+  }
 }
 
-function gameRound(){
 
-    let playerRoundWin = 0;
-    let computerRoundWin = 0;
+function computerPlay(){
+    let computerChoice = (0,1,2)
+    let randomMove = Math.floor(Math.random() * computerChoice.length);
+    if(randomMove === 0 ){
+      return "ROCK"
+    }
+    else if(randomMove === 1){
+      return "PAPER"
+    }
+    else{
+      return "SCISSORS"
+    }
 
-    while(playerRoundWin < 3 && computerRoundWin < 3){
-        let playerSelection = prompt("Please enter your choice - Rock, Paper, or Scissors ");
-        let playerSelectionCaseIS = playerSelection.toUpperCase();
-        let computerSelection = getComputerChoice();
+}
 
-        console.log("The computer picked " + computerSelection);
-
-        if(playerSelectionCaseIS === computerSelection){
-            console.log("It's a tie");
-        }
-        else if (
-            (playerSelectionCaseIS === "ROCK" && computerSelection === "SCISSORS") ||
-            (playerSelectionCaseIS === "PAPER" && computerSelection === "ROCK") ||
-            (playerSelectionCaseIS === "SCISSORS" && computerSelection === "PAPER")
-        ){
-            playerRoundWin++;
-            console.log("Congratulations! You won this round");
-        }
-        else {
-            computerRoundWin++;
-            console.log("Sorry, you lost this round! Computer won!");
-        }
-    }   
-
-    if(playerRoundWin === 3){
-        return "Congratulations You won 3 rounds first ! You won the game!";
+function updateScores() {
+    document.querySelector(".scoreP").textContent = "Player Score : " + playerScore;
+    document.querySelector(".scoreC").textContent = "Computer Score : " + computerScore;
+  }
+  function endGame() {
+    if (playerScore === 3) {
+      result.textContent = "Congratulations! You won the game!";
     } else {
-        return "Sorry, Computer made it to 3 rounds win first ! Computer won!";
+      result.textContent = "Oops! Computer won the game!";
     }
 }
 
-const result = gameRound();
-console.log(result);
+function endGame(){
+  
+}
+
